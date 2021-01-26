6b6a88048e0553e318e45dd4b9ce7c37a60fd5d6
gyara <me@gyara.moe>
2021-01-26 20:38:32 +0900

    wait until complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::convert::Infallible;
 use warp::Filter;
 use yukikaze::{send_message, Message};
 
-fn handle(msg: Message) -> &'static str {
-    tokio::task::spawn(send_message(msg));
-    "done"
+async fn handle(msg: Message) -> Result<&'static str, Infallible> {
+    send_message(msg).await;
+    Ok("done")
 }
 
 fn main() {
@@ -11,7 +12,7 @@ fn main() {
         .ok()
         .and_then(|port| port.parse().ok())
         .unwrap_or(8080);
-    let server = warp::post().and(warp::body::json()).map(handle);
+    let server = warp::post().and(warp::body::json()).and_then(handle);
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(warp::serve(server).run(([0, 0, 0, 0], port)));
 }
